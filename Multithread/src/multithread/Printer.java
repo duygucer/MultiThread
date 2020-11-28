1d3eb3c5f9ccaa0603d1bcbfb8ceca02a448f110
Duygu Üçer <32361271+duygucer@users.noreply.github.com>
2020-11-28 17:08:23 +0300

    Proje eklendi.

---
+++ Multithread/src/multithread/Printer.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package multithread;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Duygu
+ */
+public class Printer extends Thread{
+    @Override
+    public void run() {
+        System.out.println(isim+" Çalışıyor...");
+        
+        for (int i = 1; i <= 10; i++) {
+            try {
+                System.out.println(isim+ "Yazıyor : "+i);
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {
+                System.out.println("Thread kesintiye uğradı...");
+            }
+            
+        }
+        System.out.println(isim+" işini bitirdi...");
+    }
+    
+    private  String isim;
+
+    public Printer(String isim) {
+        this.isim = isim;
+    }
+
+    
+    
+}
